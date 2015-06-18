5aedf48a93f30e176e64b37eaf171f1250972edd
Roger-Wu <gsetcrw@gmail.com>
2015-06-18 23:26:41 +0800

    Add: generate lexicon.txt. Now lang directory is complete.

--- kaldi-file-prep/prepPhones.py
+++ kaldi-file-prep/prepPhones.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 MAP_FILE = sys.argv[1]  #should be "../../MLDS_Final/conf/phones.60-48-39.map"
 TIMIT_DICT_FILE = sys.argv[2]
@@ -6,26 +7,24 @@ OUTPUT_SIL_FILE = sys.argv[3]
 OUTPUT_NONSIL_FILE = sys.argv[4]
 OUTPUT_LEXICON_FILE = sys.argv[5]
 
-print MAP_FILE
-print OUTPUT_LEXICON_FILE
-
 SIL_PHONE = "sil"
 PHONES_48 = set()
 DICT_48_39 = dict()
+DICT_60_48 = dict()
 
 def main():
     readMapFile()
 
     outputSilNonsil()
-    #outputLexicon()
+    outputLexicon()
 
 def readMapFile():
     with open(MAP_FILE) as mapFile:
         for line in mapFile:
             lineList = line.rstrip().split() #split with space or tab
             
-            print lineList[1]
             PHONES_48.add(lineList[1])
+            DICT_60_48[lineList[0]] = lineList[1]
             DICT_48_39[lineList[1]] = lineList[2]
 
 def outputSilNonsil():
@@ -38,8 +37,28 @@ def outputSilNonsil():
                 nonsilFile.write(phone + '\n')
 
 def outputLexicon():
-    with open(OUTPUT_LEXICON_FILE, 'w') as lexFile:
-        pass
+    timitFile = open(TIMIT_DICT_FILE)
+    lexFile = open(OUTPUT_LEXICON_FILE, 'w')
+    
+    for line in timitFile:
+        if not line.strip():
+            continue
+        if line[0] == ';':
+            continue
+
+        pattern = '([^ ]+) *\/(.*)\/'
+        match = re.match(pattern, line)
+        word = match.group(1)
+        phones60 = match.group(2).split()
+        phones48 = list()
+        for phone in phones60:
+            phoneStrip = re.match('([a-z]*)[0-9]*', phone).group(1) # remove numbers in it
+            phones48.append(DICT_60_48[phoneStrip])
+        
+        lexFile.write(word + ' ' + ' '.join(phones48) + '\n')
+
+    timitFile.close()
+    lexFile.close()
 
 if __name__ == '__main__':
     main()
